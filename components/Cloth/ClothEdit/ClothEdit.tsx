de018c0c35b00d24c83b7f921503b00c6a589871
Vladislav <vkadsshatrov@gmail.com>
2025-04-15 16:31:56 +0500

    1

--- components/Cloth/ClothEdit/ClothEdit.tsx
+++ components/Cloth/ClothEdit/ClothEdit.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Edit, useNotify, useRedirect } from 'react-admin'
+import { Edit } from 'react-admin'
+
 import { CLOTH_SOURCE_NAME } from '../../../constants/sourceNames'
 import { useGoodsEdition } from '../../../hooks/useGoodsEdition'
 import { EditTopToolbar } from '../../elements/EditTopToolbar/EditTopToolbar'
 import { ClothForm } from '../ClothForm/ClothForm'
-import { getImagesFromRawFile } from '../../../utils/dataProvider'
 
 export const ClothEdit = () => {
   const {
@@ -15,48 +15,6 @@ export const ClothEdit = () => {
     cloneProductSpinner,
   } = useGoodsEdition(CLOTH_SOURCE_NAME)
 
-  const notify = useNotify()
-  const redirect = useRedirect()
-
-  /**
-   * Преобразует данные перед отправкой на сервер
-   * Особое внимание уделяет обработке изображений
-   */
-  const transformData = async (data: any) => {
-    try {
-      // Если есть изображения, обрабатываем их
-      if (data.images && Array.isArray(data.images)) {
-        const processedImages = await getImagesFromRawFile(data.images)
-        return {
-          ...data,
-          images: processedImages.map(img => ({
-            url: img.url,
-            title: img.title || 'Изображение товара'
-          }))
-        }
-      }
-      return data
-    } catch (error) {
-      notify('Ошибка при обработке изображений', { type: 'error' })
-      throw error // Прерываем сохранение формы
-    }
-  }
-
-  /**
-   * Обработчик успешного сохранения
-   */
-  const onSuccess = (data: any) => {
-    notify(`Товар "${data.name}" успешно обновлен`, { type: 'success' })
-    redirect('list', data.id) // Перенаправляем к списку с сохранением id
-  }
-
-  /**
-   * Обработчик ошибки сохранения
-   */
-  const onError = (error: any) => {
-    notify(`Ошибка при сохранении: ${error.message}`, { type: 'error' })
-  }
-
   return (
     <Edit
       actions={
@@ -65,11 +23,6 @@ export const ClothEdit = () => {
           spinner={cloneProductSpinner}
         />
       }
-      transform={transformData}
-      mutationOptions={{ 
-        onSuccess,
-        onError 
-      }}
     >
       <ClothForm
         type={type}
